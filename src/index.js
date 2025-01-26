2f7d8b2ad40a0bf76761224f48e20cf7fbcc9cb2
Abhijeet Singh Rathour <sabhijeet191@yahoo.com>
2025-01-27 00:00:52 +0530

    Initial commit to new repository

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./app/store";
+import App from "./App";
+
+import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
+
+import "./index.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <ProductsProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </ProductsProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { BrowserRouter } from 'react-router-dom';
+
+// import App from './App';
+// import { UserProvider } from './contexts/user.context';
+// import { ProductsProvider } from './contexts/products.context';
+// import { CartProvider } from './contexts/cart.context';
+
+// import './index.scss';
+
+// const rootElement = document.getElementById('root');
+
+// render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <UserProvider>
+//         <ProductsProvider>
+//           <CartProvider>
+//             <App />
+//           </CartProvider>
+//         </ProductsProvider>
+//       </UserProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   rootElement
+// );
